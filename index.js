2555d095eb8d3e6d130514f3a09afefe8ee311bb
Mohamed1204 <chabayta-93@hotmail.com>
2017-09-22 13:12:51 +0200

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const path = __dirname + '/public/'; // path til mappen, + public da html filer ligger i public mappen
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello from root</h1>');
+});
+
+app.get('/testdigselv', function(req, res){
+    res.send('du vandt');
+});
+
+app.get('/login', function(req, res){
+    res.sendFile(path +'login.html', function(err){
+        // handle error
+    });
+});
+
+app.post('/login', function (req, res) {
+    console.log(req.body.user);
+    console.log(req.body.pass);
+
+    
+    if(req.body.user === 'clbo'){
+    
+
+    res.sendFile(path +'verysecret.html', function (err) {
+    });
+    }else{
+        res.sendFile(path +'login.html', function(err){
+            // handle error
+        });
+    }    
+});
+
+app.listen(3000);
+
+
+
+/*var http = require('http');
+http.createServer(function(req, res){
+});*/
